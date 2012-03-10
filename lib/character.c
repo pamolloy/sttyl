e69bd8a037bec19391ce617a433e57036b5d6f36
Philip Molloy <philip.a.molloy@gmail.com>
2012-03-09 21:00:15 -0500

    Fix header, move structs, formatting
    Edit the file header for setting.c to reflect file contents. Minor
     table formatting changes. Move structure declarations in setting.c
     and character.c ahead of forward function declarations.

--- lib/character.c
+++ lib/character.c
@@ -4,8 +4,8 @@
  * DATA STRUCTURES
  *	A structure containing the constant that defines the element in the
  *	cc_c array, a corresponding name, and pointers to printer and
- *	setter functions.
- *		spl( constant, name, print, set )
+ *	setter functions
+ *		spl( constant, name, printer, setter )
  *
  *	An array of option structures that each contain a special character
  *		splchars[]
@@ -30,23 +30,37 @@
 #include <stddef.h>		// NULL
 #include <stdio.h>		// printf(), putchar()
 
-void printspl( struct spl *row, struct termios *ttyopts );
-int setspl( struct spl *row, struct termios *ttyopts, char *opt);
-
 struct spl {
 	tcflag_t constant;
 	char *name;
-	void (*print)(struct spl *, struct termios *);
-	void (*set)(struct spl *, struct termios *, char *);
+	void (*printer)(struct spl *, struct termios *);
+	void (*setter)(struct spl *, struct termios *, char *);
 };
+
+void printspl( struct spl *row, struct termios *ttyopts );
+int setspl( struct spl *row, struct termios *ttyopts, char *opt);
 
 struct spl splchars[] = {
-	{ VINTR	, "intr"	, printspl	, setspl },
-	{ VERASE, "erase"	, printspl	, setspl },
-	{ VKILL	, "kill"	, printspl	, setspl },
-	{ 0		, NULL		, 0			, 0 },
+	{ VINTR	, "intr"	, printspl	, setspl	},
+	{ VERASE, "erase"	, printspl	, setspl	},
+	{ VKILL	, "kill"	, printspl	, setspl	},
+	{ 0		, NULL		, 0			, 0			},
 };
 
+int setspl( struct spl *row, struct termios *ttyopts, char *opt)
+{
+	ttyopts->c_cc[row->constant] = *opt;
+
+	if (tcsetattr(FD, TCSANOW, ttyopts) == -1) {
+		perror("tcsetattr");
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * TODO
+ */
 void printc( int c )
 {
 	if ( isprint(c) != 0 ) {
@@ -62,16 +76,5 @@ void printspl( struct spl *row, struct termios *ttyopts )
 	printf(row->name);
 	printf(" = ");
 	printc(ttyopts->c_cc[row->constant]);
-}
-
-int setspl( struct spl *row, struct termios *ttyopts, char *opt)
-{
-	ttyopts->c_cc[row->constant] = *opt;
-
-	if (tcsetattr(FD, TCSANOW, ttyopts) == -1) {
-		perror("tcsetattr");
-		return -1;
-	}
-	return 0;
 }
 
--- lib/setting.c
+++ lib/setting.c
@@ -1,20 +1,27 @@
 /*
- * lib/speed.c - functions to print and set the baud speed
+ * lib/setting.c - functions to print and set the input, output, and
+ * local settings
  *
  * DATA STRUCTURES
- *	A structure containing the constant, which represents the baud
- *	speed and a corresponding name.
- *		spd( constant, name)
+ *	A structure containing the constant, which defines the setting, a
+ *	corresponding name, and pointers to printer and setter functions
+ *		opt( constant, name, printer, setter )
  *
- *	An array of speed structures that each contain an available speed
- *		baud_spds[]
+ *	An array of "opt" structures that each contain an available device
+ *	option
+ *		devopts[]
  *
  * FUNCTIONS
- *	Print the baud speed stored in the termios structure
- *		printspd( struct termios )
+ *	Print whether an option stored in the termios structure is on or
+ *	off using the stty syntax
+ *		p_in( row, ttyopts )	// Print input setting
+ *		p_out( row, ttyopts )	// Print output setting
+ *		p_local( row, ttyopts )	// Print local settings
  *
- *	Set the baud speed based on a name assigned in the speed structure
- *		setspd( struct termios, speed )
+ *	Set the device option setting
+ *		s_in( row, ttyopts, on )	// Set input setting
+ *		s_out( row, ttyopts, on )	// Set output setting
+ *		s_local( row, ttyopts, on )	// Set local setting
  */
 
 #include "../sttyl.h"	// FD
@@ -23,21 +30,21 @@
 #include <string.h>		// strcmp()
 #include <stdio.h>		// printf()
 
-void s_in( struct option *row, struct termios *ttyopts, int on );
-void s_out( struct option *row, struct termios *ttyopts, int on );
-void s_local( struct option *row, struct termios *ttyopts, int on );
-void p_in( struct option *row, struct termios *ttyopts );
-void p_out( struct option *row, struct termios *ttyopts );
-void p_local( struct option *row, struct termios *ttyopts );
-
-struct option {
+struct opt {
 	tcflag_t constant;
 	char *name;
-	void (*printer)(struct option *, struct termios *);
-	void (*setter)(struct option *, struct termios *, int);
+	void (*printer)(struct opt *, struct termios *);
+	void (*setter)(struct opt *, struct termios *, int);
 };
 
-struct option devopts[] = {
+void s_in( struct opt *row, struct termios *ttyopts, int on );
+void s_out( struct opt *row, struct termios *ttyopts, int on );
+void s_local( struct opt *row, struct termios *ttyopts, int on );
+void p_in( struct opt *row, struct termios *ttyopts );
+void p_out( struct opt *row, struct termios *ttyopts );
+void p_local( struct opt *row, struct termios *ttyopts );
+
+struct opt devopts[] = {
 	{ ICRNL		, "icrnl"	, p_in		, s_in		},
 	{ ONLCR		, "onlcr"	, p_out		, s_out		},
 	{ OLCUC		, "olcuc"	, p_out		, s_out		},
@@ -48,9 +55,10 @@ struct option devopts[] = {
 	{ 0			, NULL		, 0			, 0			}
 };
 
-/* SET */
-
-int s_opt( struct option *row, struct termios *ttyopts, tcflag_t *flag_p, int on )
+/*
+ * TODO
+ */
+int s_opt( struct opt *row, struct termios *ttyopts, tcflag_t *flag_p, int on )
 {
 	if ( on == 1 )
 		*flag_p |= row->constant;
@@ -64,24 +72,25 @@ int s_opt( struct option *row, struct termios *ttyopts, tcflag_t *flag_p, int on
 	return 0;
 }
 
-void s_in( struct option *row, struct termios *ttyopts, int on )
+void s_in( struct opt *row, struct termios *ttyopts, int on )
 {
 	s_opt(row, ttyopts, &ttyopts->c_iflag, on);
 }
 
-void s_out( struct option *row, struct termios *ttyopts, int on )
+void s_out( struct opt *row, struct termios *ttyopts, int on )
 {
 	s_opt(row, ttyopts, &ttyopts->c_oflag, on);
 }
 
-void s_local( struct option *row, struct termios *ttyopts, int on )
+void s_local( struct opt *row, struct termios *ttyopts, int on )
 {
 	s_opt(row, ttyopts, &ttyopts->c_lflag, on);
 }
 
-/* PRINT */
-
-void p_opt ( struct option *row, int flag)
+/*
+ * TODO
+ */
+void p_opt ( struct opt *row, int flag)
 {
 	if ( flag & row->constant )
 		printf(row->name);
@@ -89,17 +98,17 @@ void p_opt ( struct option *row, int flag)
 		printf("-%s", row->name);
 }
 
-void p_in( struct option *row, struct termios *ttyopts )
+void p_in( struct opt *row, struct termios *ttyopts )
 {
 	p_opt(row, ttyopts->c_iflag);
 }
 
-void p_out( struct option *row, struct termios *ttyopts )
+void p_out( struct opt *row, struct termios *ttyopts )
 {
 	p_opt(row, ttyopts->c_oflag);
 }
 
-void p_local( struct option *row, struct termios *ttyopts )
+void p_local( struct opt *row, struct termios *ttyopts )
 {
 	p_opt(row, ttyopts->c_lflag);
 }
