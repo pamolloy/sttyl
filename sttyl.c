ff8c934e1ac3dbb50e7998df3f06964f3c8b5f91
Philip Molloy <philip.a.molloy@gmail.com>
2012-03-10 14:01:50 -0500

    Split up argument parsing into functions

--- sttyl.c
+++ sttyl.c
@@ -1,13 +1,17 @@
 /*
- * TODO
+ * sttyl - TODO
  */
 
-#include "sttyl.h"	// FD
 #include <termios.h>
 #include <stdio.h>	// perror()
 #include <stdlib.h>	// exit()
 #include <string.h>	// strcmp()
 #include <ctype.h>	// isdigit()
+
+#include "sttyl.h"	// FD
+#include "character.c"
+#include "special.c"
+#include "speed.c"
 
 int main( int ac, char **av )
 {
@@ -19,39 +23,68 @@ int main( int ac, char **av )
 		exit(1);
 	}
 
+	parse_args(ac, av);
+
+	exit(0);
+}
+
+void parse_args( int ac, char **av)
+{
 	if ( ac <= 1 ) {
 		print_speed(&ttyopts);				// Print the speed
 		print_special(&ttyopts);			// Print all special characters
 		loop_setting(&ttyopts, NULL, 0);	// Print all settings
 	}
-	else {		// Parse for input/output/local setting, special character, or speed
+	else {
 		int i;
 		for (i = 1; i < ac; i++) {
-			//TODO Create is_speed function
-			if ( isdigit(av[i]) != 0 ) {	// If it's a digit then
-				set_speed(&ttyopts, av[i]);	// Set the speed
+			if ( check_speed(av[i]) == 0 )
 				continue;
-			}
-			int special_i = is_special(av[i]);	// If argument is special character
-			if ( special_i != -1 ) {
-				if ( set_special(&ttyopts, special_i, av[i + 1]) == -1 ) {	// Set special character
-					fprintf(stderr, "%s: Invalid integer argument \
-							`%s'\n", program_name, av[i + 1]);
-					exit(1);
-				}
+			if ( check_special(av[i], av[i] + 1) == 0 ) {
 				i++;
 				continue;
 			}
-			/*TODO Follow above pattern? (i.e. check if argument is a
-			 * input/output/local setting, then set it?)
-			 */
-			if (av[i][0] == '-')
-				loop_setting(&ttyopts, av[i] + 1, 1);
-			else
-				loop_opts(&ttyopts, av[i], 0);
+			if ( check_setting(av[i]) == 0 )
+				continue;
+			fprintf(stderr, "%s: invalid argument `%s'", program_name, av[i]);
 		}
 	}
+}
 
-	exit(0);
+//TODO The following functions have not been tested
+
+int check_speed( char *av )
+{
+	if ( is_speed(av[i]) != -1 ) {
+		set_speed(&ttyopts, av[i]);
+		return 0;
+	}
+	return -1;
+}
+
+int check_special( char *av, char *next)
+{
+	int i = is_special(av[i]);
+	if ( i != -1 ) {
+		if ( set_special(&ttyopts, i, next) == -1 ) {
+			fprintf(stderr, "%s: Invalid integer argument \
+					`%s'\n", program_name, next);
+			exit(1);
+		}
+		return 0;
+	}
+	return -1;
+}
+
+int check_setting( char *av )
+{
+	int i = is_setting(av[i]);
+	if ( i != -1 ) {
+		if (av[i][0] == '-')
+			set_setting(&ttyopts, av + 1, 1);
+		else
+			set_setting(&ttyopts, av, 0);
+	}
+	return -1;
 }
 
