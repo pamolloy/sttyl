8ab5bca1860a14a3e0ba4900e8cc50a924fb115b
Philip Molloy <philip.a.molloy@gmail.com>
2012-03-13 18:17:21 -0400

    Add function declarations, documentation
    Add a comment to the file header documenting its contents. Add
     declarations for functions called by main.c. Add conditional statements
     for the definition of FD, the file descriptor used by tcsetattr().

--- defs.h
+++ defs.h
@@ -1,6 +1,24 @@
 /*
- * TODO
+ * defs.h - function declarations, and file descriptor definitions
  */
 
-#define FD 0	// Assume standard input
-//TODO tcsetattr() argument
+#ifndef FD
+#define FD 0	// Assume standard input for all calls to tcsetattr()
+#endif
+
+#ifndef DEFS_H
+#define DEFS_H
+/* speed.c */
+int is_speed( char *av );
+void set_speed( struct termios *ttyopts, int i );
+void print_speed( struct termios *ttyopts );
+/* special.c */
+int is_special( char *av );
+int set_special( struct termios *ttyopts, int i, char *c);
+void print_special( struct termios *ttyopts );
+/* setting.c */
+int is_setting( char *av );
+void set_setting( struct termios *ttyopts, int i, int on );
+void print_setting( struct termios *ttyopts );
+#endif
+
